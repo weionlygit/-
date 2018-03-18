b6c01e50f39ffcf4b031fccd1d46fa5553400968
Weipeng <13546782483@163.com>
2018-03-18 11:55:48 +0800

    毕设主页3月18日小框架跳转

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    var a=document.getElementsByClassName("main-wrapper-playboy");
+
+    var b=document.getElementsByClassName("login");
+    b.onclick=function(){
+        a.className="main-wrapper-login";
+    }
+}
